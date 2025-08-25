8c68ee06e5a1533a7f9f84d3a03df14b3300d53a
Nursultan Mj <nursuccessmj@gmail.com>
2025-08-25 21:10:19 +0600

    added getModule,getLesson functions

--- src/Store/CoursesStore.ts
+++ src/Store/CoursesStore.ts
@@ -1,14 +1,19 @@
 import { create } from "zustand";
-import type { Course, Module } from "../types";
+import type { Course, Lesson, Module } from "../types";
 import axios from "axios";
+import { Try } from "@mui/icons-material";
 
 interface State {
   courses: Course[];
   modules: Module[];
-  selectedModuleId: number|null;
+  lessons: Lesson[];
+  selectedModuleId: number | null;
   getCourses: () => Promise<void>;
+  getModules: (current_pk: number) => Promise<void>;
+  getLessons: (module_pk: number) => Promise<void>;
 }
-export const useCourseStore = create<State>((set, get ) => {
+
+export const useCourseStore = create<State>((set, get) => {
   return {
     courses: [],
     getCourses: async () => {
@@ -16,15 +21,38 @@ export const useCourseStore = create<State>((set, get ) => {
         const { data } = await axios.get("http://localhost:3000/courses");
         set({ courses: data });
       } catch (e) {
-       console.error("Failed to fetch courses:", e);
+        console.error("Failed to fetch courses:", e);
       }
     },
-    
+    getModules: async (course_pk) => {
+      try {
+        const { data } = await axios.get<Module[]>(
+          `http://localhost:3000/courses/${course_pk}/modules`
+        );
+        set({ modules: data });
+      } catch (e) {
+        console.error("Failed to fetch modules:", e);
+      }
+    },
+    getLessons: async (module_pk) => {
+      try {
+        const { data } = await axios.get<Lesson[]>(
+          `http://localhost:3000/courses/${module_pk}/modules`
+        );
+        set({ lessons: data });
+      } catch (e) {
+        console.error("Failed to fetchlessons:", e);
+      }
+    },
   };
 });
 export const useCourses = () => useCourseStore((store) => store.courses);
 
 export const useCoursesAction = () => {
   const store = useCourseStore();
-  return { getCourses: store.getCourses };
+  return {
+    getCourses: store.getCourses,
+    getModules: store.getModules,
+    getLessons: store.getLessons,
+  };
 };
