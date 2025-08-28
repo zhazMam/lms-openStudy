ae43782d2f754252ec97715898970eb91eb3d4e1
Nursultan Mj <nursuccessmj@gmail.com>
2025-08-29 01:20:57 +0600

    store for signup & login

---
+++ src/Store/AuthStore.ts
@@ -0,0 +1,88 @@
+import { create } from "zustand";
+import type { User } from "../types";
+import axios from "axios";
+
+interface State {
+  users: User[];
+  signUp: (newUser: object) => Promise<boolean>;
+  logout: () => void;
+  token: string | null;
+  isAuth: Boolean;
+  login: (user: object) => Promise<boolean>;
+  justSignedUp: Boolean;
+}
+export const useAuthStore = create<State>((set) => {
+  return {
+    users: [],
+    token: localStorage.getItem("accessToken") || null,
+    isAuth: !!localStorage.getItem("accessToken"),
+    justSignedUp: localStorage.getItem("justSignedUp") === "true",
+
+    signUp: async (newUser) => {
+      console.log(newUser);
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3000/register",
+          newUser
+        );
+        if (data.accessToken) {
+          set({
+            token: data.accessToken,
+            isAuth: true,
+            justSignedUp: true,
+          });
+          localStorage.setItem("accessToken", data.accessToken);
+          localStorage.setItem("justSignedUp", "true");
+          return true;
+        }
+        return false;
+      } catch (e) {
+        if (axios.isAxiosError(e) && e.response) {
+          throw e.response.data;
+        } else {
+          throw new Error("An unknown error occurred during sign up.");
+        }
+      }
+    },
+    login: async (User) => {
+      console.log(User);
+      try {
+        const { data } = await axios.post("http://localhost:3000/login", User);
+        if (data.accessToken) {
+          set({
+            token: data.accessToken,
+            isAuth: true,
+            justSignedUp: false,
+          });
+          localStorage.setItem("accessToken", data.accessToken);
+          localStorage.setItem("justSignedUp", "false");
+          return true;
+        }
+        return false;
+      } catch (e) {
+        if (axios.isAxiosError(e) && e.response) {
+          throw e.response.data;
+        } else {
+          throw new Error("An unknown error occurred during login.");
+        }
+      }
+    },
+    logout: () => {
+      set({ token: null, isAuth: false, justSignedUp: false });
+      localStorage.removeItem("accessToken");
+    },
+  };
+});
+export const useToken = () => useAuthStore((store) => store.token);
+export const useIsAuth = () => useAuthStore((store) => store.isAuth);
+export const useJustSignedUp = () =>
+  useAuthStore((store) => store.justSignedUp);
+
+export const useAuthAction = () => {
+  const store = useAuthStore();
+  return {
+    signUp: store.signUp,
+    logout: store.logout,
+    login: store.login,
+  };
+};
